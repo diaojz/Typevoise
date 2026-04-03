b7b24f9dd80ceb25241d2ca08d476b9269e292c0
diaojz </>
2026-04-03 16:38:50 +0800

    Initial Commit

---
+++ Typevoise/TypevoiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypevoiseApp.swift
+//  Typevoise
+//
+//  Created by diaoye on 2026/4/3.
+//
+
+import SwiftUI
+
+@main
+struct TypevoiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
